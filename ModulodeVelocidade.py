835df2f1e9206c2471679b87bffe8bb6f92b783f
Michelson <70553280+7michelson@users.noreply.github.com>
2021-12-07 15:05:24 -0300

    Update ModulodeVelocidade.py

--- ModulodeVelocidade.py
+++ ModulodeVelocidade.py
@@ -2,9 +2,9 @@ from math import pow
 from math import sqrt
 
 
-vx=(-51.0-(-47.3))/(2020-1999)
-vy=(-29.5-(-30.0))/(2020-1999)
-V=sqrt(pow(vx, 2)+ pow(vy, 2))
+vx = (-51.0-(-47.3))/(2020-1999)
+vy = (-29.5-(-30.0))/(2020-1999)
+V = sqrt(pow(vx, 2)+ pow(vy, 2))
 print(vx,'long/anoS')
 print(vy, 'lati/anos')
 print(V, 'graus/anos')
