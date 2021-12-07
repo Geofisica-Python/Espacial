ffd4003d408701593fce3429980b4c10a144cc01
Michelson <70553280+7michelson@users.noreply.github.com>
2021-12-07 15:05:49 -0300

    Update ModulodeVelocidade.py

--- ModulodeVelocidade.py
+++ ModulodeVelocidade.py
@@ -1,7 +1,7 @@
 from math import pow
 from math import sqrt
 
-
+#Velocidade de deriva da anomalia
 vx = (-51.0-(-47.3))/(2020-1999)
 vy = (-29.5-(-30.0))/(2020-1999)
 V = sqrt(pow(vx, 2)+ pow(vy, 2))
